8c5d81df2bfe7567133e70503a4e428ab6c5947d
dongwoo <73861795+ornwoo96@users.noreply.github.com>
2025-02-14 18:58:53 +0900

    두 수 연산 값 비교하기

--- AlgorithmTest/Programmers/Level0/CompareOperationResultTest.swift
+++ AlgorithmTest/Programmers/Level0/CompareOperationResultTest.swift
@@ -6,3 +6,13 @@
 //
 
 
+import Testing
+
+struct CompareOperationResultTest {
+    
+    
+    @Test func test() {
+        #expect(solution_CompareOperationResult(2, 91) == 364)
+        #expect(solution_CompareOperationResult(91, 2) == 912)
+    }
+}
--- Source/Programmers/Level0/CompareOperationResult.swift
+++ Source/Programmers/Level0/CompareOperationResult.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
-func solution(_ a:Int, _ b:Int) -> Int {
-    return 0
+func solution_CompareOperationResult(_ a:Int,
+              _ b:Int) -> Int {
+    return max(Int("\(a)\(b)")!,2*a*b)
 }
